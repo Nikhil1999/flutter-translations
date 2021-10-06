819c123483cf585022dcc89183974fa31a5c4fd0
Nikhil1999 <nikhilkoshty@gmail.com>
2021-10-07 00:25:31 +0530

    Added download flow for translations

---
+++ lib/config/app_config.dart
@@ -0,0 +1,8 @@
+class AppConfig {
+  AppConfig._();
+
+  static const String flutterTranslationFileName = 'flutter_translations.zip';
+  static const String androidTranslationFileName = 'android_translations.zip';
+
+  static const List<String> allowedExtensions = ['csv'];
+}
